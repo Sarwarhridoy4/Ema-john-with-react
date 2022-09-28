76af28790197bd91afa6bdc732fe2da472b7e364
Sarwarhridoy4 <sarwarhridoy4@gmail.com>
2022-09-28 08:04:06 +0600

    ...

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -14,7 +14,7 @@ const Shop = () => {
   }, []);
 
   const handelAddToCart = (product) => {
-    console.log(product);
+    // console.log(product);
     const newCart = [...cart, product];
     setCart(newCart);
   };
